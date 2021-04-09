a05c20d2f5c8d095b72483e9e5bb2616b01e0031
Júnior Nunes <denisonprobable13@gmail.com>
2021-04-09 18:43:51 -0300

    The user list is visible and clickable

--- src/data/users.js
+++ src/data/users.js
@@ -4,27 +4,27 @@ export default [
         name: 'João Silva',
         email: 'josil@empmail.com',
         avatarURL:
-            'https://cdn.pixabay.com/photo/2016/08/31/11/54/user-1633249_960_720.png'        
+            'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png'        
     },
     {
         id: 2,
         name: 'Mariana Aguiar',
         email: 'rimasil@empmail.com',
         avatarURL:
-            'https://cdn.pixabay.com/photo/2016/08/31/11/54/user-1633249_960_720.png'        
+            'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png'        
     },
     {
         id: 3,
         name: 'Julia Fragoso',
         email: 'juliafral@empmail.com',
         avatarURL:
-            'https://cdn.pixabay.com/photo/2016/08/31/11/54/user-1633249_960_720.png'        
+            'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png'        
     },
     {
         id: 4,
         name: 'Rafael Monteiro',
         email: 'rafamontee@empmail.com',
         avatarURL:
-            'https://cdn.pixabay.com/photo/2016/08/31/11/54/user-1633249_960_720.png'        
+            'https://cdn.pixabay.com/photo/2021/03/25/08/43/child-6122408_960_720.png'        
     },
 ];
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import { ListItem, Avatar } from 'react-native-elements';
+import users from '../data/users';
 
 export default props => {
+
+    function getUserItem({ item: user }){
+        return (
+            <ListItem 
+                key={user.id}
+                bottomDivider
+                onPress={() => props.navigation.navigate('UserForm')} 
+            >
+                <Avatar source={{uri: user.avatarURL}} />
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        )
+    }
+
     return (
-        <Text>UserList</Text>
+        <View>
+            <FlatList 
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
     )
 }
