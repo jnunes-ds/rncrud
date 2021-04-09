7433c424d1bc9265df94e17f0216b547322b40f7
Júnior Nunes <denisonprobable13@gmail.com>
2021-04-09 18:07:52 -0300

    Mocked users created

---
+++ src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: 1,
+        name: 'João Silva',
+        email: 'josil@empmail.com',
+        avatarURL:
+            'https://cdn.pixabay.com/photo/2016/08/31/11/54/user-1633249_960_720.png'        
+    },
+    {
+        id: 2,
+        name: 'Mariana Aguiar',
+        email: 'rimasil@empmail.com',
+        avatarURL:
+            'https://cdn.pixabay.com/photo/2016/08/31/11/54/user-1633249_960_720.png'        
+    },
+    {
+        id: 3,
+        name: 'Julia Fragoso',
+        email: 'juliafral@empmail.com',
+        avatarURL:
+            'https://cdn.pixabay.com/photo/2016/08/31/11/54/user-1633249_960_720.png'        
+    },
+    {
+        id: 4,
+        name: 'Rafael Monteiro',
+        email: 'rafamontee@empmail.com',
+        avatarURL:
+            'https://cdn.pixabay.com/photo/2016/08/31/11/54/user-1633249_960_720.png'        
+    },
+];
