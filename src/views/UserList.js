f7541416019fc5bb85282217f1289ccedcfbd1f8
Júnior Nunes <denisonprobable13@gmail.com>
2021-04-09 21:09:14 -0300

    Created buttons for editing and deleting users

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { ListItem, Avatar } from 'react-native-elements';
+import { View, Text, FlatList, Alert } from 'react-native';
+import { ListItem, Avatar, Icon } from 'react-native-elements';
+import { Button } from 'react-native-elements/dist/buttons/Button';
 import users from '../data/users';
 
 export default props => {
+
+    function confirmUserDelection(user){
+        Alert.alert('Excluir usuário', 'Deseja expluir o usuário?', [
+            {
+                text: 'Sim',
+                onPress(){
+                    console.warn('delete' + user.id)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
 
     function getUserItem({ item: user }){
         return (
             <ListItem 
                 key={user.id}
                 bottomDivider
-                onPress={() => props.navigation.navigate('UserForm')} 
+                onPress={() => props.navigation.navigate('UserForm', user)} 
             >
                 <Avatar source={{uri: user.avatarURL}} />
                 <ListItem.Content>
                     <ListItem.Title>{user.name}</ListItem.Title>
                     <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
                 </ListItem.Content>
+                <ListItem.Chevron
+                    onPress={() => navigation.navigate('userForm', user)}
+                    iconProps={{name: "edit"}}
+                    iconStyle={{fontSize: 25, color: "orange"}}
+                />
+                <ListItem.Chevron
+                    onPress={() => confirmUserDelection(user)}
+                    iconProps={{name: 'delete'}}
+                    iconStyle={{fontSize: 25, color: "red"}}
+                />
             </ListItem>
         )
     }
